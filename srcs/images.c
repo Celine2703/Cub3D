15efbeba34cc4f3361f9f7c7fba4c9799f849087
cmartin <you@example.com>
2023-10-19 06:36:15 +0200

    petit

--- srcs/images.c
+++ srcs/images.c
@@ -75,30 +75,51 @@ int main()
     int line_bytes;
     int endian;
     char *buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
-
-    int color = 0xABCDEF;
+    // printf("pixel_bits: %d\n", pixel_bits);
+    // printf("line_bytes: %d\n", line_bytes);
+    // printf("endian: %d\n", endian);
+    int color = 0xFFFFFF;
+    int color2 = 0x000000;
 
     if (pixel_bits != 32)
         color = mlx_get_color_value(mlx, color);
 
     for(int y = 0; y < 360; ++y)
-    for(int x = 0; x < 640; ++x)
     {
-        int pixel = (y * line_bytes) + (x * 4);
+        for(int x = 0; x < 640; ++x)
+        {
+            int pixel = (y * line_bytes) + (x * 4);
 
-        if (endian == 1)        // Most significant (Alpha) byte first
-        {
-            buffer[pixel + 0] = (color >> 24);
-            buffer[pixel + 1] = (color >> 16) & 0xFF;
-            buffer[pixel + 2] = (color >> 8) & 0xFF;
-            buffer[pixel + 3] = (color) & 0xFF;
-        }
-        else if (endian == 0)   // Least significant (Blue) byte first
-        {
-            buffer[pixel + 0] = (color) & 0xFF;
-            buffer[pixel + 1] = (color >> 8) & 0xFF;
-            buffer[pixel + 2] = (color >> 16) & 0xFF;
-            buffer[pixel + 3] = (color >> 24);
+            if (endian == 1)        // Most significant (Alpha) byte first
+            {
+                buffer[pixel + 0] = (color >> 24);
+                buffer[pixel + 1] = (color >> 16) & 0xFF;
+                buffer[pixel + 2] = (color >> 8) & 0xFF;
+                buffer[pixel + 3] = (color) & 0xFF;
+            }
+            else if (endian == 0)   // Least significant (Blue) byte first
+            {
+                buffer[pixel + 0] = (color) & 0xFF;
+                buffer[pixel + 1] = (color >> 8) & 0xFF;
+                buffer[pixel + 2] = (color >> 16) & 0xFF;
+                buffer[pixel + 3] = (color >> 24);
+            }
+            if (endian == 1 && y > 300)        // Most significant (Alpha) byte first
+            {
+                
+                buffer[pixel + 0] = (color2 >> 24);
+                buffer[pixel + 1] = (color2 >> 16) & 0xFF;
+                buffer[pixel + 2] = (color2 >> 8) & 0xFF;
+                buffer[pixel + 3] = (color2) & 0xFF;
+            }
+            else if (endian == 0 && y > 300)   // Least significant (Blue) byte first
+            {
+                buffer[pixel + 0] = (color2) & 0xFF;
+                buffer[pixel + 1] = (color2 >> 8) & 0xFF;
+                buffer[pixel + 2] = (color2 >> 16) & 0xFF;
+                buffer[pixel + 3] = (color2 >> 24);
+            }
+
         }
     }
     mlx_put_image_to_window(mlx, win, image, 0, 0);
