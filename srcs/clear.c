590e1c239a0c1f6c36e6a2754b2ebd7d8dd4fdcc
cmartin <you@example.com>
2023-10-27 10:00:41 +0200

    début events + clear

---
+++ srcs/clear.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmartin <cmartin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 08:30:38 by cmartin           #+#    #+#             */
+/*   Updated: 2023/10/27 08:35:47 by cmartin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	ft_mapclear(t_map *map)
+{
+    int	i;
+
+    map ->map_width = 0;
+    map ->map_height = 0;
+    if (!map ->map)
+        return ;
+    i = 0;
+    while (map ->tab[i])
+    {
+        free(map ->tab[i]);
+        i++;
+    }
+    free(map ->tab);
+}
+
+void    ft_stop(t_map *map, void *mlx)
+{
+    ft_mapclear(map);
+    mlx_destroy_display(mlx);
+    free(mlx);
+}
+
+void    ft_destroy(t_data *data)
+{
+    ft_mapclear(data.map);
+    mlx_destroy_window(data->mlx, data->win);
+    mlx_destroy_display(data->mlx);
+    free(data->mlx);
+    exit(EXIT_SUCCESS);
+}
--- srcs/events.c
+++ srcs/events.c
@@ -6,17 +6,44 @@
 /*   By: cmartin <cmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 22:14:59 by cmartin           #+#    #+#             */
-/*   Updated: 2023/10/26 22:18:05 by cmartin          ###   ########.fr       */
+/*   Updated: 2023/10/27 09:47:07 by cmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d.h"
 
-//envoyer t_data, si touche z -> avance : changer data->player->pos(x ou y)
-int	ft_key(int key, t_game *game)
+void    ft_putmov(t_player player, double angle){
+    if (player->angle >= 0 && player->angle <= M_PI/2)
+        player->posx += cos(player->angle)*0.2;
+        player->posy += sin(player->angle)*0.2;
+    else if (player->angle >= M_PI/2 && player->angle < M_PI)
+        player->posx -= cos(player->angle)*0.2;
+        player->posy += sin(player->angle)*0.2;
+    else if (player->angle >= M_PI && player->angle < 3*M_PI/2)
+        player->posx -= cos(player->angle)*0.2;
+        player->posy -= sin(player->angle)*0.2;
+    else if (player->angle >= 3*M_PI/2 && player->angle <= 2*M_PI)
+        player->posx += cos(player->angle)*0.2;
+        player->posy -= sin(player->angle)*0.2;
+}
+
+int ft_mov(t_map map, t_player player, int key){
+    if(key == 'w')
+        ft_putmov(player, player->angle);
+    else if(key == 'a')
+        ft_putmov(player, player->angle + M_PI/2);
+    else if(key == 's')
+        ft_putmov(player, player->angle + M_PI);
+    else if(key == 'd')
+        ft_putmov(player, player->angle + 3*M_PI/2);
+    return (0);
+}
+
+int ft_key(int key, t_data *data)
 {
-	// if (key == 65307)
-	// 	ft_destroy(game);
-	ft_mov(game, &pos, key);
-	ft_display_bis(game ->map, game ->mlx, &pos);
-	return (0);
+    if (key == 65307)
+        ft_destroy(data);
+    ft_mov(data.map, data.player, key);
+    ft_display_bis(data.map, data->mlx, &pos);
+    return (0);
 }
