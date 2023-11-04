c9789454169d07aeab3365516d12acec025c4c5c
clement.cravero <clement.cravero@gmail.com>
2023-11-04 14:14:10 +0100

    script

---
+++ script.sh
@@ -0,0 +1,18 @@
+./cub3D
+valgrind ./cub3D
+printf "\n"
+# ./cub3D "./map/2_elements.cub"
+# valgrind ./cub3D "./map/2_elements.cub"
+# printf "\n"
+# ./cub3D "./map/EA_miss.cub"
+# valgrind ./cub3D "./map/EA_miss.cub"
+# printf "\n"
+# ./cub3D "./map/empty.cub"
+# valgrind ./cub3D "./map/empty.cub"
+# printf "\n"
+# ./cub3D ./map/invalid_char.cub
+# valgrind ./cub3D ./map/invalid_char.cub
+# printf "\n"
+# ./cub3D ./map/laby.cub
+# valgrind ./cub3D ./map/laby.cub
+# printf "\n"
